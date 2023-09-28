504ed3466d0f686f7f6f8f7a13e4bb6b0badaf49
developer-vsandhu <77821789+developervinay1@users.noreply.github.com>
2023-09-28 10:20:10 +0530

    Navigation and Login Added

--- StackNavigator.js
+++ StackNavigator.js
@@ -4,12 +4,13 @@ import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import LoginScreen from "./screens/LoginScreen";
 import RegisterScreen from "./screens/RegisterScreen";
+import HomeScreen from "./screens/HomeScreen";
 
 export default function StackNavigator() {
   const Stack = createNativeStackNavigator();
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="Register">
+      <Stack.Navigator initialRouteName="Login">
         <Stack.Screen
           name="Login"
           component={LoginScreen}
@@ -18,6 +19,11 @@ export default function StackNavigator() {
         <Stack.Screen
           name="Register"
           component={RegisterScreen}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name="Home"
+          component={HomeScreen}
           options={{ headerShown: false }}
         />
       </Stack.Navigator>
--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -4,11 +4,91 @@ import {
   SafeAreaView,
   TextInput,
   TouchableOpacity,
+  Alert,
+  ActivityIndicator,
 } from "react-native";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { themeColor } from "../theme";
+import { onAuthStateChanged, signInWithEmailAndPassword } from "firebase/auth";
+import { auth } from "../firebase";
+import { useNavigation } from "@react-navigation/native";
 
 export default function LoginScreen() {
+  const navigation = useNavigation();
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    setLoading(true);
+    const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
+      if (!currentUser) {
+        setLoading(false);
+      }
+      if (currentUser) {
+        navigation.replace("Home");
+      }
+    });
+  }, []);
+
+  const logInUser = () => {
+    if (email === "" || password === "") {
+      Alert.alert(
+        "Inavlid Details",
+        "Please fill all the details properly.",
+        [
+          {
+            text: "Cancel",
+            style: "default",
+          },
+          {
+            text: "Ok",
+            style: "default",
+          },
+        ],
+        { cancelable: true }
+      );
+    } else {
+      signInWithEmailAndPassword(auth, email, password)
+        .then((userCredentials) => {
+          const user = userCredentials.user;
+          Alert.alert(
+            "Login Success",
+            "Your account has been Logged In.",
+            [
+              {
+                text: "Cancel",
+                style: "cancel",
+              },
+              {
+                text: "Ok",
+                style: "default",
+              },
+            ],
+            { cancelable: true }
+          );
+          navigation.replace("Home");
+        })
+        .catch((error) => {
+          Alert.alert(
+            "Error",
+            error.message,
+            [
+              {
+                text: "Cancel",
+                style: "default",
+              },
+              {
+                text: "Ok",
+                style: "default",
+              },
+            ],
+            { cancelable: true }
+          );
+        });
+    }
+  };
+
   return (
     <SafeAreaView
       style={{
@@ -17,84 +97,101 @@ export default function LoginScreen() {
         justifyContent: "center",
       }}
     >
-      <View style={{ paddingHorizontal: 30 }}>
+      {loading ? (
         <View>
-          <Text style={{ fontSize: 20, fontWeight: 600, textAlign: "center" }}>
-            Login To Continue
-          </Text>
+          <ActivityIndicator size={"large"} color={themeColor.darkColor} />
         </View>
-        <View
-          style={{
-            borderWidth: 1,
-            marginTop: 30,
-            paddingVertical: 20,
-            borderRadius: 5,
-            backgroundColor: themeColor.grayColor,
-            borderColor: themeColor.grayColor,
-            paddingHorizontal: 30,
-          }}
-        >
-          <TextInput
-            placeholder="Email"
-            autoCapitalize="none"
-            autoCorrect={false}
-            autoComplete="off"
-          />
-        </View>
-        <View
-          style={{
-            borderWidth: 1,
-            marginTop: 20,
-            paddingVertical: 20,
-            borderRadius: 5,
-            backgroundColor: themeColor.grayColor,
-            borderColor: themeColor.grayColor,
-            paddingHorizontal: 30,
-          }}
-        >
-          <TextInput
-            secureTextEntry
-            placeholder="Password"
-            autoCapitalize="none"
-            autoCorrect={false}
-            autoComplete="off"
-          />
-        </View>
-
-        <TouchableOpacity
-          style={{
-            borderWidth: 1,
-            marginTop: 20,
-            paddingVertical: 20,
-            borderRadius: 10,
-            backgroundColor: themeColor.darkColor,
-            borderColor: themeColor.darkColor,
-            paddingHorizontal: 30,
-          }}
-        >
-          <Text
-            style={{ textAlign: "center", color: "white", fontWeight: "bold" }}
+      ) : (
+        <View style={{ paddingHorizontal: 30 }}>
+          <View>
+            <Text
+              style={{ fontSize: 20, fontWeight: 600, textAlign: "center" }}
+            >
+              Login To Continue
+            </Text>
+          </View>
+          <View
+            style={{
+              borderWidth: 1,
+              marginTop: 30,
+              paddingVertical: 20,
+              borderRadius: 5,
+              backgroundColor: themeColor.grayColor,
+              borderColor: themeColor.grayColor,
+              paddingHorizontal: 30,
+            }}
           >
-            Login
-          </Text>
-        </TouchableOpacity>
+            <TextInput
+              placeholder="Email"
+              autoCapitalize="none"
+              autoCorrect={false}
+              autoComplete="off"
+              onChangeText={(text) => setEmail(text)}
+            />
+          </View>
+          <View
+            style={{
+              borderWidth: 1,
+              marginTop: 20,
+              paddingVertical: 20,
+              borderRadius: 5,
+              backgroundColor: themeColor.grayColor,
+              borderColor: themeColor.grayColor,
+              paddingHorizontal: 30,
+            }}
+          >
+            <TextInput
+              secureTextEntry
+              placeholder="Password"
+              autoCapitalize="none"
+              autoCorrect={false}
+              autoComplete="off"
+              onChangeText={(text) => setPassword(text)}
+            />
+          </View>
 
-        <View
-          style={{
-            flexDirection: "row",
-            justifyContent: "space-between",
-            alignItems: "center",
-            marginTop: 20,
-          }}
-        >
-          <Text style={{ fontWeight: 600, fontSize: 16 }}>
-            Don't have an account?{" "}
-          </Text>
-          <TouchableOpacity>
-            <Text style={{ fontWeight: 600, fontSize: 16 }}>Register Now</Text>
+          <TouchableOpacity
+            onPress={logInUser}
+            style={{
+              borderWidth: 1,
+              marginTop: 20,
+              paddingVertical: 20,
+              borderRadius: 10,
+              backgroundColor: themeColor.darkColor,
+              borderColor: themeColor.darkColor,
+              paddingHorizontal: 30,
+            }}
+          >
+            <Text
+              style={{
+                textAlign: "center",
+                color: "white",
+                fontWeight: "bold",
+              }}
+            >
+              Login
+            </Text>
           </TouchableOpacity>
+
+          <View
+            style={{
+              flexDirection: "row",
+              justifyContent: "space-between",
+              alignItems: "center",
+              marginTop: 20,
+            }}
+          >
+            <Text style={{ fontWeight: 600, fontSize: 16 }}>
+              Don't have an account?{" "}
+            </Text>
+            <TouchableOpacity onPress={() => navigation.navigate("Register")}>
+              <Text style={{ fontWeight: 600, fontSize: 16 }}>
+                Register Now
+              </Text>
+            </TouchableOpacity>
+          </View>
         </View>
-      </View>
+      )}
     </SafeAreaView>
   );
 }
